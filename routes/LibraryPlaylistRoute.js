10c21708ebbf97bca628d028ee7ff64b4d47bb68
abhishek tiwari <89748622+abhishekjmd@users.noreply.github.com>
2023-01-25 10:38:17 +0530

    creating library server

--- routes/LibraryPlaylistRoute.js
+++ routes/LibraryPlaylistRoute.js
@@ -5,7 +5,7 @@ const { createLibraryPlaylist, pushMoviesToLibraryPlaylist, getLibrary, deleteLi
 Router.post('/addLibrary', createLibraryPlaylist);
 Router.put('/updateLibrary/:id', pushMoviesToLibraryPlaylist);
 Router.put('updateLibrary/:id/movies/:id', pushMoviesToLibraryPlaylist);
-Router.delete('deleteLibrary', deleteLiraryPlaylist);
+Router.delete('deleteLibrary/:id', deleteLiraryPlaylist);
 Router.get('/getLibrary', getLibrary);
 
 module.exports = Router
