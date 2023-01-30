3a5197900a18da7aa22058f38d33f0f2c074dd3a
abhishek tiwari <89748622+abhishekjmd@users.noreply.github.com>
2023-01-30 09:15:59 +0530

    added authentication servers

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const movieRoute = require('./routes/moviesRoute')
 const topPlaylistRoute = require('./routes/topPlaylistRoute')
 const artistRoute = require('./routes/artistRoute')
 const libraryPlaylistRoute = require('./routes/LibraryPlaylistRoute')
+const userRoute = require('./routes/userRoute')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/multiflex', movieRoute);
 app.use('/playlist', topPlaylistRoute);
-app.use('/artist', artistRoute)
-app.use('/library', libraryPlaylistRoute)
+app.use('/artist', artistRoute);
+app.use('/library', libraryPlaylistRoute);
+app.use('/user', userRoute);
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => res.send('Hello World!'))
